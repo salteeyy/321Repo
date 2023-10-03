b656e6d9435f58ead3983efbb5ad5862e5f320bb
Salt <manapsalty@gmail.com>
2023-10-03 08:33:40 -0400

    THIS IS OUR FIRST COMMIT

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test{
+    static int id;
+    static String name;
+    static boolean started;
+
+    public void method1(String name){
+        
+    }
+
+    public void method2(int id){
+
+    }
+
+    public void method3(boolean started){
+
+    }
+
+}
